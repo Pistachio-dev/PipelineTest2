8d885343a9b91289e10cb8d5e048c435c5acebf8
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2025-05-16 15:36:40 +0200

    Dalamudbasics is now referenced by the plugin

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,10 +1,11 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using SamplePlugin.Windows;
+using DalamudBasics.Targeting;
 
 namespace SamplePlugin;
 
@@ -28,6 +29,7 @@ public sealed class Plugin : IDalamudPlugin
     public Plugin()
     {
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+        ITargetingService targetingService = null;
 
         // you might normally want to embed resources and load them from the manifest stream
         var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
